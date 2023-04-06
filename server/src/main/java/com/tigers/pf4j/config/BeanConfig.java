dd0229ebd2143e9f8dc722393c01de6561583b01
L Jackie <1343729568@qq.com>
2023-04-06 18:20:42 +0800

    feat: 加强一些注释说明

--- server/src/main/java/com/tigers/pf4j/config/BeanConfig.java
+++ server/src/main/java/com/tigers/pf4j/config/BeanConfig.java
@@ -9,7 +9,9 @@ import javax.annotation.Resource;
 
 /**
  * {@code BeanConfig}
- *
+ * 这里直接从 {@link PluginManager} 中拿，
+ * 当然如果要动态加载，还需要一些更上层的封装了，暂时没有想到
+ * 很好的办法。
  * @author Tigers Leung
  * @version 1.0
  * @since 1.0
@@ -20,6 +22,11 @@ public class BeanConfig {
     @Resource
     private PluginManager pluginManager ;
 
+    /**
+     * 直接从 PluginManager 里面拿。
+     * {@link com.tigers.pf4j.PluginManagerAdapter}
+     * @return 返回 LoginService 的对应扩展。
+     */
     @Bean
     public LoginService loginService ()
     {
